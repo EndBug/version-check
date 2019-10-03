4febe91dad6f8b92dd68e4294c0281f64e25cba5
Federico Grandi <fgrandi30@gmail.com>
2019-10-03 19:55:04 +0200

    stuff

--- src/main.ts
+++ src/main.ts
@@ -190,9 +190,9 @@ async function checkDiff(sha: string, version: string) {
       added: (versionLines.added.match(semverRegex()) || [])[0],
       deleted: !!versionLines.deleted && (versionLines.deleted.match(semverRegex()) || [])[0]
     }
+    console.log(versionLines)
     console.log(versionLines.added.match(semverRegex()))
     console.log(versions)
-    console.log(version)
     if (versions.added != version) return false
     console.log('check ok')
 
