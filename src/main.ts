4b8074b50488c9a0e455d3e3ed5244a6580bfa67
Federico Grandi <fgrandi30@gmail.com>
2020-04-16 21:11:57 +0200

    Make some order

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
-import * as core from '@actions/core'
+import { getInput, setFailed, info, error, warning } from '@actions/core'
 import axios from 'axios'
 import { readFile } from 'fs'
 import { join } from 'path'
 import semverDiff from 'semver-diff'
 import semverRegex from 'semver-regex'
 
-const packageFileName = core.getInput('file-name') || 'package.json',
+const packageFileName = getInput('file-name') || 'package.json',
   dir = process.env.GITHUB_WORKSPACE || '/github/workspace',
   eventFile = process.env.GITHUB_EVENT_PATH || '/github/workflow/event.json',
-  token = core.getInput('token')
+  token = getInput('token')
 
 type ArgValue<T> =
   T extends 'changed' ? boolean :
@@ -16,10 +16,194 @@ type ArgValue<T> =
   T extends 'version' ? string | undefined :
   never
 
+// #region Functions
 async function main() {
   const eventObj = await readJson(eventFile)
-  const commits = eventObj.commits || await getCommits(eventObj.pull_request._links.commits.href)
+  const commits = eventObj.commits || await request(eventObj.pull_request._links.commits.href)
   return await processDirectory(dir, commits)
+}
+
+async function readJson(file: string) {
+  const data: string = await new Promise((resolve, reject) =>
+    readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(data)
+      }
+    })
+  )
+  return JSON.parse(data)
+}
+
+async function request(url: string) {
+  const headers = token ? {
+    Authorization: `Bearer ${token}`
+  } : undefined
+  return (await axios.get(url, { headers })).data
+}
+
+async function processDirectory(dir: string, commits: LocalCommit[] | PartialCommitResponse[]) {
+  try {
+    const packageFile = join(dir, packageFileName),
+      packageObj = await readJson(packageFile).catch(() => {
+        Promise.reject(
+          new NeutralExitError(`Package file not found: ${packageFile}`)
+        )
+      })
+
+    if (!isPackageObj(packageObj))
+      throw new Error('Can\'t find version field')
+
+    if (commits.length >= 20)
+      warning('This workflow run topped the commit limit set by GitHub webhooks: that means that commits could not appear and that the run could not find the version change.')
+
+    await checkCommits(commits, packageObj.version)
+  } catch (e) {
+    setFailed(`${e}`)
+  }
+}
+
+async function checkCommits(commits: LocalCommit[] | PartialCommitResponse[], version: string) {
+  try {
+    for (const commit of commits) {
+      const { message, sha } = getBasicInfo(commit)
+      const match = message.match(semverRegex()) || []
+      if (match.includes(version)) {
+        if (await checkDiff(sha, version)) {
+          info(`Found match for version ${version}: ${sha.substring(0, 7)} ${message}`)
+          return true
+        }
+      }
+    }
+
+    if (getInput('diff-search')) {
+      info('No standard npm version commit found, switching to diff search (this could take more time...)')
+
+      for (const commit of commits) {
+        const { message, sha } = getBasicInfo(commit)
+
+        if (await checkDiff(sha, version)) {
+          info(`Found match for version ${version}: ${sha.substring(0, 7)} ${message}`)
+          return true
+        }
+      }
+    }
+
+    info('No matching commit found.')
+    return false
+  } catch (e) {
+    setFailed(`${e}`)
+  }
+}
+
+function getBasicInfo(commit: LocalCommit | PartialCommitResponse) {
+  let message: string,
+    sha: string
+
+  if (isLocalCommit(commit)) {
+    message = commit.message
+    sha = commit.id
+  } else {
+    message = commit.commit.message
+    sha = commit.sha
+  }
+
+  return {
+    message,
+    sha
+  }
+}
+
+async function checkDiff(sha: string, version: string) {
+  try {
+    const commit = await getCommit(sha)
+    const pkg = commit.files.find(f => f.filename == packageFileName)
+    if (!pkg) return false
+
+    const versionLines: {
+      added?: string
+      deleted?: string
+    } = {}
+
+    const rawLines = pkg.patch.split('\n')
+      .filter(line => line.includes('"version":') && ['+', '-'].includes(line[0]))
+    if (rawLines.length > 2) return false
+
+    for (const line of rawLines)
+      versionLines[line.startsWith('+') ? 'added' : 'deleted'] = line
+    if (!versionLines.added) return false
+
+    const versions = {
+      added: matchVersion(versionLines.added),
+      deleted: !!versionLines.deleted && matchVersion(versionLines.deleted)
+    }
+    if (versions.added != version) return false
+
+    setOutput('changed', true)
+    setOutput('version', version)
+    if (versions.deleted)
+      setOutput('type', semverDiff(versions.deleted, versions.added))
+    return true
+  } catch (e) {
+    error(`An error occurred in checkDiff:\n${e}`)
+    throw new ExitError(1)
+  }
+}
+
+async function getCommit(sha: string): Promise<CommitResponse> {
+  const url = `https://api.github.com/repos/${process.env.GITHUB_REPOSITORY}/commits/${sha}`
+  return request(url)
+}
+
+function matchVersion(str: string) {
+  return ((str.split('"') || [])
+    .map(s => s.match(semverRegex()))
+    .find(e => !!e) || [])[0]
+}
+
+function setOutput<T extends 'changed' | 'type' | 'version'>(name: T, value: ArgValue<T>) {
+  // @ts-ignore
+  return setOutput(name, `${value}`)
+}
+// #endregion
+
+// #region Error classes
+class ExitError extends Error {
+  code?: number
+
+  constructor(code: number | null) {
+    super(`Command failed with code ${code}`)
+    if (typeof code == 'number') this.code = code
+  }
+}
+
+class NeutralExitError extends Error { }
+// #endregion
+
+if (require.main == module) {
+  info('Searching for version update...')
+  main().catch(e => {
+    if (e instanceof NeutralExitError) process.exitCode = 78
+    else {
+      process.exitCode = 1
+      error(e.message || e)
+    }
+  })
+}
+
+// #region Types and interfaces
+interface CommitResponse extends PartialCommitResponse {
+  files: {
+    filename: string
+    additions: number
+    deletions: number
+    changes: number
+    status: string
+    raw_url: string
+    blob_url: string
+    patch: string
+  }[]
 }
 
 interface LocalCommit {
@@ -32,7 +216,6 @@ interface LocalCommit {
   url: string
   distinct: boolean
 }
-
 function isLocalCommit(value): value is LocalCommit {
   return typeof value.id == 'string'
 }
@@ -42,36 +225,6 @@ interface PackageObj {
 }
 function isPackageObj(value): value is PackageObj {
   return !!value && !!value.version
-}
-
-async function getCommits(commitsUrl: string): Promise<PartialCommitResponse[]> {
-  core.info('debug: using url: ' + commitsUrl)
-  const headers = token ? {
-    Authorization: `Bearer ${token}`,
-    // Accept: 'application/vnd.github.v3+json'
-  } : undefined
-  return (await axios.get(commitsUrl, { headers })).data
-}
-
-async function getCommit(sha: string): Promise<CommitResponse> {
-  const url = `https://api.github.com/repos/${process.env.GITHUB_REPOSITORY}/commits/${sha}`
-  const headers = token ? {
-    Authorization: `Bearer ${token}`
-  } : undefined
-  return (await axios.get(url, { headers })).data
-}
-
-interface CommitResponse extends PartialCommitResponse {
-  files: {
-    filename: string
-    additions: number
-    deletions: number
-    changes: number
-    status: string
-    raw_url: string
-    blob_url: string
-    patch: string
-  }[]
 }
 
 interface PartialCommitResponse {
@@ -155,158 +308,4 @@ interface PartialCommitResponse {
     total: number
   }
 }
-
-function getBasicInfo(commit: LocalCommit | PartialCommitResponse) {
-  let message: string,
-    sha: string
-
-  if (isLocalCommit(commit)) {
-    message = commit.message
-    sha = commit.id
-  } else {
-    message = commit.commit.message
-    sha = commit.sha
-  }
-
-  return {
-    message,
-    sha
-  }
-}
-
-async function checkCommits(commits: LocalCommit[] | PartialCommitResponse[], version: string) {
-  try {
-    for (const commit of commits) {
-      const { message, sha } = getBasicInfo(commit)
-      const match = message.match(semverRegex()) || []
-      if (match.includes(version)) {
-        if (await checkDiff(sha, version)) {
-          console.log(`Found match for version ${version}: ${sha.substring(0, 7)} ${message}`)
-          return true
-        }
-      }
-    }
-
-    if (core.getInput('diff-search')) {
-      console.log('No standard npm version commit found, switching to diff search (this could take more time...)')
-
-      for (const commit of commits) {
-        const { message, sha } = getBasicInfo(commit)
-
-        if (await checkDiff(sha, version)) {
-          console.log(`Found match for version ${version}: ${sha.substring(0, 7)} ${message}`)
-          return true
-        }
-      }
-    }
-
-    console.log('No matching commit found.')
-    return false
-  } catch (e) {
-    core.setFailed(`${e}`)
-  }
-}
-
-async function checkDiff(sha: string, version: string) {
-  try {
-    const commit = await getCommit(sha)
-    const pkg = commit.files.find(f => f.filename == packageFileName)
-    if (!pkg) return false
-
-    const versionLines: {
-      added?: string
-      deleted?: string
-    } = {}
-
-    const rawLines = pkg.patch.split('\n')
-      .filter(line => line.includes('"version":') && ['+', '-'].includes(line[0]))
-    if (rawLines.length > 2) return false
-
-    for (const line of rawLines)
-      versionLines[line.startsWith('+') ? 'added' : 'deleted'] = line
-    if (!versionLines.added) return false
-
-    const versions = {
-      added: matchVersion(versionLines.added),
-      deleted: !!versionLines.deleted && matchVersion(versionLines.deleted)
-    }
-    if (versions.added != version) return false
-
-    setOutput('changed', true)
-    setOutput('version', version)
-    if (versions.deleted)
-      setOutput('type', semverDiff(versions.deleted, versions.added))
-    return true
-  } catch (e) {
-    console.error(`An error occurred in checkDiff:\n${e}`)
-    throw new ExitError(1)
-  }
-}
-
-function matchVersion(str: string) {
-  return ((str.split('"') || [])
-    .map(s => s.match(semverRegex()))
-    .find(e => !!e) || [])[0]
-}
-
-async function processDirectory(dir: string, commits: LocalCommit[] | PartialCommitResponse[]) {
-  try {
-    const packageFile = join(dir, packageFileName),
-      packageObj = await readJson(packageFile).catch(() => {
-        Promise.reject(
-          new NeutralExitError(`Package file not found: ${packageFile}`)
-        )
-      })
-
-    if (!isPackageObj(packageObj))
-      throw new Error('Can\'t find version field')
-
-    if (commits.length >= 20)
-      console.warn('This workflow run topped the commit limit set by GitHub webhooks: that means that commits could not appear and that the run could not find the version change.')
-
-    await checkCommits(commits, packageObj.version)
-  } catch (e) {
-    core.setFailed(`${e}`)
-  }
-}
-
-async function readJson(file: string) {
-  const data: string = await new Promise((resolve, reject) =>
-    readFile(file, 'utf8', (err, data) => {
-      if (err) {
-        reject(err)
-      } else {
-        resolve(data)
-      }
-    })
-  )
-  return JSON.parse(data)
-}
-
-function setOutput<T extends 'changed' | 'type' | 'version'>(name: T, value: ArgValue<T>) {
-  return core.setOutput(name, `${value}`)
-}
-
-// #region Error classes
-class ExitError extends Error {
-  code?: number
-
-  constructor(code: number | null) {
-    super(`Command failed with code ${code}`)
-    if (typeof code == 'number') this.code = code
-  }
-}
-
-class NeutralExitError extends Error { }
 // #endregion
-
-if (require.main == module) {
-  console.log('Searching for version update...')
-  main().catch(e => {
-    if (e instanceof NeutralExitError) process.exitCode = 78
-    else {
-      process.exitCode = 1
-      console.error(e.message || e)
-    }
-  })
-}
