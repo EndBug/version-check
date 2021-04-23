bdb672599148c68c9c9606e831f1480247097431
Federico Grandi <fgrandi30@gmail.com>
2021-04-23 08:34:01 +0200

    fix: fix path in readLocalJson

--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,11 @@
 import { Toolkit } from 'actions-toolkit'
+import path from 'path'
 
 export const tools = new Toolkit({ event: ['push'] })
 
-export function readLocalJson(filePath: string) {
+export function readLocalJson(localPath: string) {
   try {
-    const data = tools.readFile(filePath)
+    const data = tools.readFile(path.join(__dirname, localPath))
     if (typeof data == 'string') return JSON.parse(data)
   } catch (e) {
     return undefined
