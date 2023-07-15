b58504e3ddd07f51e020a9dfdddbf18eab74fb3a
Federico Grandi <fgrandi30@gmail.com>
2023-07-15 12:58:58 +0200

    chore: fix linters cnfiguration

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
-    'prettier',
+    'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended'
   ],
   globals: {},
